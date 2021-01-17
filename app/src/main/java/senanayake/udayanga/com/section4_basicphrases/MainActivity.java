c61e9a04c16141698f09d178f84c24b18fb5634e
u-senanayake <mailtoudayanga@gmail.com>
2021-01-17 21:12:58 +0900

    Added sound clips play by buttons

--- app/src/main/java/senanayake/udayanga/com/section4_basicphrases/MainActivity.java
+++ app/src/main/java/senanayake/udayanga/com/section4_basicphrases/MainActivity.java
@@ -2,7 +2,10 @@ package senanayake.udayanga.com.section4_basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void buttonClicked(View view) {
+
+        int id = view.getId();
+        String name = "";
+
+        name = view.getResources().getResourceEntryName(id);
+
+
+        int resourceId = getResources().getIdentifier(name, "raw", getPackageName());
+
+        MediaPlayer player = MediaPlayer.create(this, resourceId);
+        player.start();
+
+        Log.i("Button taped", name);
+        Log.i("Resource ID", Integer.toString(resourceId));
     }
 }
